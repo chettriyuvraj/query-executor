cd70035db46d74093daff6605f44feb1673b3a9e
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-02-26 19:14:39 +0530

    1. feat: test file scan operator by plugging it in 2. assets: add movies.csv

--- main.go
+++ main.go
@@ -4,27 +4,45 @@ import "fmt"
 
 func main() {
 
-	table := Table{
-		headers: []string{"id", "name", "genre"},
-		data: []map[string]interface{}{
-			{"id": 1, "name": "Lion King", "genre": "Comedy"},
-			{"id": 2, "name": "Psycho", "genre": "Horror"},
-			{"id": 3, "name": "Chaplin", "genre": "Comedy"},
-			{"id": 4, "name": "American Horror Story", "genre": "Thriller"},
-		},
-	}
+	// table := Table{
+	// 	headers: []string{"id", "name", "genre"},
+	// 	data: []map[string]interface{}{
+	// 		{"id": 1, "name": "Lion King", "genre": "Comedy"},
+	// 		{"id": 2, "name": "Psycho", "genre": "Horror"},
+	// 		{"id": 3, "name": "Chaplin", "genre": "Comedy"},
+	// 		{"id": 4, "name": "American Horror Story", "genre": "Thriller"},
+	// 	},
+	// }
+
+	// qd := QueryDescriptor{
+	// 	cmd:  COMMANDS["SELECT"],
+	// 	text: "SELECT id, genre from movies LIMIT 2",
+	// 	planNode: &LimitNode{
+	// 		limit: 2,
+	// 		inputs: []PlanNode{
+	// 			&ProjectionNode{
+	// 				reqHeaders: []string{"id", "genre"},
+	// 				inputs: []PlanNode{
+	// 					&TableScanNode{
+	// 						table: table,
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
 
 	qd := QueryDescriptor{
 		cmd:  COMMANDS["SELECT"],
-		text: "SELECT id, genre from movies LIMIT 2",
+		text: "SELECT movieId, genres from movies LIMIT 3",
 		planNode: &LimitNode{
-			limit: 2,
+			limit: 3,
 			inputs: []PlanNode{
 				&ProjectionNode{
-					reqHeaders: []string{"id", "genre"},
+					reqHeaders: []string{"movieId", "genres"},
 					inputs: []PlanNode{
-						&TableScanNode{
-							table: table,
+						&FileScanNode{
+							path: "/Users/yuvrajchettri/Desktop/Development/query-executor/assets/movies.csv",
 						},
 					},
 				},
