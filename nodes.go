9e036b10de21b7a4987b380f954c9912dd74bc5c
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-02-26 16:12:04 +0530

    1. feat: create nodes

---
+++ nodes.go
@@ -0,0 +1,107 @@
+package main
+
+import "fmt"
+
+var COMMANDS = map[string]string{
+	"SELECT": "select",
+}
+
+type Tuple struct {
+	data map[string]interface{}
+}
+
+type PlanNode interface { /* This is the iterator interface */
+	init() error
+	next() (Tuple, error)
+	close() error
+	getInputs() []*PlanNode
+}
+
+type ScanNode interface {
+}
+
+/*** Table ***/
+
+type Table struct {
+	headers []string
+	data    []map[string]interface{} // assume data will always contain keys from headers only
+}
+
+func (t *Table) getData(idx int) map[string]interface{} {
+	if idx >= len(t.data) {
+		return nil
+	}
+	return t.data[idx]
+}
+
+/*** Table Scan Node ***/
+
+type TableScanNode struct {
+	reqHeaders []string
+	table      Table
+	tableIdx   int
+	inputs     []*PlanNode
+}
+
+func (tn *TableScanNode) init() error {
+	return nil
+}
+
+func (tn *TableScanNode) next() (Tuple, error) {
+	// Get data from table
+	data := tn.table.getData(tn.tableIdx)
+	tuple := Tuple{data: data}
+
+	// Confirm if all headers exist
+	for _, header := range tn.reqHeaders {
+		_, exists := data[header]
+		if !exists {
+			return Tuple{}, fmt.Errorf("header doesn't exist in table")
+		}
+	}
+
+	// increase table index count
+	if data != nil {
+		tn.tableIdx++
+	}
+
+	return tuple, nil
+}
+
+func (tn *TableScanNode) close() error {
+	return nil
+}
+
+func (tn *TableScanNode) getInputs() ([]*PlanNode, error) {
+	return tn.inputs, nil
+}
+
+/*** Limit Node ***/
+
+type LimitNode struct {
+	offset int
+	limit  int
+	inputs []*PlanNode
+}
+
+func (ln *LimitNode) init() error {
+	return nil
+}
+
+func (ln *LimitNode) next() (Tuple, error) {
+	tuple := Tuple{}
+
+	if ln.offset >= ln.limit {
+		return tuple, nil
+	}
+
+	return (*ln.inputs[0]).next()
+}
+
+func (ln *LimitNode) close() error {
+	return nil
+}
+
+func (ln *LimitNode) getInputs() ([]*PlanNode, error) {
+	return ln.inputs, nil
+}
