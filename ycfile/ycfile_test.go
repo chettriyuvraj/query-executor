f4196e3e4bc2d9422c86ffbd538165c98179ff74
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-02-28 21:58:11 +0530

    1. feat: test read write

--- ycfile/ycfile_test.go
+++ ycfile/ycfile_test.go
@@ -43,3 +43,40 @@ func TestCreateYCFile(t *testing.T) {
 	require.Equal(t, fieldTypes, fieldTypesHeader)
 	require.Equal(t, expectedFieldsHeader, fieldsHeader)
 }
+
+func TestReadWrite(t *testing.T) {
+	fields := []string{"movieId", "title", "genres"}
+	fieldTypes := []byte{0, 2, 1} // ss, sl, sm
+	path := "../assets/movies"
+
+	err := CreateYCFile(path, fields, fieldTypes)
+	require.NoError(t, err)
+
+	r1 := YCFileRecord{data: []StringPair{{key: "movieId", val: "123"}, {key: "title", val: "Love You Zindagi"}, {key: "genres", val: "Romance"}}}
+	r2 := YCFileRecord{data: []StringPair{{key: "movieId", val: "124"}, {key: "title", val: "Sholay"}, {key: "genres", val: "Comedy | Romance"}}}
+	r3 := YCFileRecord{data: []StringPair{{key: "movieId", val: "125"}, {key: "title", val: "Chole"}, {key: "genres", val: "Food | Thriller"}}}
+
+	writer, err := NewYCFileWriter(path)
+	require.NoError(t, err)
+	defer writer.Close()
+	err = writer.Write(r1)
+	require.NoError(t, err)
+	err = writer.Write(r2)
+	require.NoError(t, err)
+	err = writer.Write(r3)
+	require.NoError(t, err)
+
+	reader, err := NewYCFileReader(path)
+	require.NoError(t, err)
+	defer reader.Close()
+	r1Read, err := reader.Read()
+	require.NoError(t, err)
+	r2Read, err := reader.Read()
+	require.NoError(t, err)
+	r3Read, err := reader.Read()
+	require.NoError(t, err)
+
+	require.Equal(t, r1, r1Read)
+	require.Equal(t, r2, r2Read)
+	require.Equal(t, r3, r3Read)
+}
