471f010a1101a691ea8d9b7f4ebb517f86a63ae0
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-03-04 03:20:30 +0530

    1. test: make testing of nest joins table driven

--- joins_test.go
+++ joins_test.go
@@ -6,40 +6,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNaiveNestedJoin(t *testing.T) {
+func TestNestedJoin(t *testing.T) {
 	pathMovies := "./assets/moviessmall.csv"
 	pathRatings := "./assets/ratings.csv"
+	/* Common components */
+	qd := QueryDescriptor{cmd: COMMANDS["SELECT"], text: "SELECT AVG(r.rating) FROM movies m, ratings r WHERE r.movie_id = m.id AND r.movie_id = 1;",
+		planNode: &AvgNode{header: "rating"},
+	}
+	csvNodeMovies, csvNodeRatings := &CSVScanNode{path: pathMovies}, &CSVScanNode{path: pathRatings}
+	filterNodeRatings := &FilterNode{header: "movieId", operator: "=", cmpValue: "1", inputs: []PlanNode{csvNodeRatings}}
+	nestedJoinInputs := []PlanNode{csvNodeMovies, filterNodeRatings}
 
-	qd := QueryDescriptor{
-		cmd:  COMMANDS["SELECT"],
-		text: "SELECT AVG(r.rating) FROM movies m, ratings r WHERE r.movie_id = m.id AND r.movie_id = 1;",
-		planNode: &AvgNode{
-			header: "rating",
-			inputs: []PlanNode{
-				&NaiveNestedJoinNode{
-					headers: []string{"movieId", "movieId"},
-					inputs: []PlanNode{
-						&CSVScanNode{
-							path: pathMovies,
-						},
-						&FilterNode{
-							header:   "movieId",
-							operator: "=",
-							cmpValue: "1",
-							inputs: []PlanNode{
-								&CSVScanNode{
-									path: pathRatings,
-								},
-							},
-						},
-					},
-				},
-			},
+	/* Defining and running test cases */
+	tc := []struct {
+		nestedJoinNode PlanNode
+	}{
+		{
+			nestedJoinNode: &NaiveNestedJoinNode{headers: []string{"movieId", "movieId"}},
 		},
 	}
-	qe := QueryExecutor{}
-	res, err := qe.ExecutePlan(&qd)
 
-	require.NoError(t, err)
-	require.Equal(t, res[0].data["average"], 3.921239561324077)
+	for _, test := range tc {
+		test.nestedJoinNode.setInputs(nestedJoinInputs)
+		qd.planNode.setInputs([]PlanNode{test.nestedJoinNode})
+		qe := QueryExecutor{}
+		res, err := qe.ExecutePlan(&qd)
+
+		require.NoError(t, err)
+		require.Equal(t, res[0].data["average"], 3.921239561324077)
+	}
 }
